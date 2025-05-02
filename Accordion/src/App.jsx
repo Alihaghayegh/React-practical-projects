4dd79453138b618583eed158195604774c2dcccc
alihaghayegh <haghayeghali95@gmail.com>
2025-05-02 21:09:33 +0330

    Accordions added

---
+++ Accordion/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import { accordionData } from "./utils/contents";
+
+const App = () => {
+  return (
+    <div>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
